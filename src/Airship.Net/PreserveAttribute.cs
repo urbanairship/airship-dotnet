40aacca2d800351da6b8814a4a1ddce8bbccd271
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2023-07-24 15:32:07 -0700

    Set isTrimmable false for iOS, add some Preserve flags

---
+++ src/Airship.Net/PreserveAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AirshipDotNet
+{
+    /// Adds `Preserve` attribute support to control the linker.
+    [AttributeUsage(AttributeTargets.All)]
+    public sealed class PreserveAttribute : Attribute
+    {
+        public bool AllMembers;
+        public bool Conditional;
+    }
+}
+
