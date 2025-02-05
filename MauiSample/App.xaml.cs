6a4b8977e28d9d20a7d5dd97a36cf20be93eaca6
khmMouna <mouna.khemiri@airship.com>
2025-02-05 15:34:21 +0100

    Add deeplink navigation to sample app

--- MauiSample/App.xaml.cs
+++ MauiSample/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace MauiSample;
+﻿using System;
+using AirshipDotNet;
+
+namespace MauiSample;
 
 public partial class App : Application
 {
@@ -7,6 +10,41 @@ public partial class App : Application
 		InitializeComponent();
 
 		MainPage = new AppShell();
-	}
+
+        // Register deep link event handler
+        Airship.Instance.OnDeepLinkReceived += OnDeepLinkReceived;
+    }
+
+    private void OnDeepLinkReceived(object sender, DeepLinkEventArgs e)
+    {
+        Uri uri = new Uri(e.DeepLink);
+        Console.WriteLine("Deeplink Received! uri = " + uri);
+
+        if (uri.Host.ToLower() == "deeplink")
+        {
+            var components = uri.AbsolutePath.ToLower().Split(separator:"/", StringSplitOptions.RemoveEmptyEntries);
+            if (components.First() != null) {
+                switch (components.First())
+                {
+                    case "home":
+                        ((AppShell)App.Current.MainPage).SwitchtoTab(Tabs.homeTab);
+                        return;
+
+                    case "inbox":
+                        ((AppShell)App.Current.MainPage).SwitchtoTab(Tabs.inboxTab);
+                        return;
+
+                    case "settings":
+                        ((AppShell)App.Current.MainPage).SwitchtoTab(Tabs.settingsTab);
+                        return;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        Console.WriteLine("App does not know how to handle deepLink" + uri);
+    }
 }
 
