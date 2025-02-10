8d5b4ada1fee1d5c3307e4a7adf29d1132699b59
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2025-02-10 13:09:16 -0800

    Bump to Android SDK 19.1.0 and get it building

--- binderator/source/com.urbanairship.android/urbanairship-message-center/Additions/MessageListFragment.cs
+++ binderator/source/com.urbanairship.android/urbanairship-message-center/Additions/MessageListFragment.cs
@@ -10,27 +10,27 @@ namespace UrbanAirship.MessageCenter
 {
 	public partial class MessageListFragment
 	{
-		public void GetAbsListViewAsync(Action<AbsListView> absListView)
-		{
-			GetAbsListViewAsync(new OnListViewReadyCallback(absListView));
-		}
-
-		internal class OnListViewReadyCallback : Java.Lang.Object, IOnListViewReadyCallback
-		{
-			Action<AbsListView> onListViewReady;
-
-			public OnListViewReadyCallback(Action<AbsListView> onListViewReady)
-			{
-				this.onListViewReady = onListViewReady;
-			}
-
-			public void OnListViewReady(AbsListView absListView)
-			{
-				if (absListView != null)
-				{
-					onListViewReady.Invoke(absListView);
-				}
-			}
-		}
+		// public void GetAbsListViewAsync(Action<AbsListView> absListView)
+		// {
+		// 	GetAbsListViewAsync(new OnListViewReadyCallback(absListView));
+		// }
+		//
+		// internal class OnListViewReadyCallback : Java.Lang.Object, IOnListViewReadyCallback
+		// {
+		// 	Action<AbsListView> onListViewReady;
+		//
+		// 	public OnListViewReadyCallback(Action<AbsListView> onListViewReady)
+		// 	{
+		// 		this.onListViewReady = onListViewReady;
+		// 	}
+		//
+		// 	public void OnListViewReady(AbsListView absListView)
+		// 	{
+		// 		if (absListView != null)
+		// 		{
+		// 			onListViewReady.Invoke(absListView);
+		// 		}
+		// 	}
+		// }
 	}
 }
