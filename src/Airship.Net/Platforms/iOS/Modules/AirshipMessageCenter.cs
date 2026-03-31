a37d98e5af41f6de4e6281723552b853a70439d9
David <jdavidcrow@gmail.com>
2026-03-30 20:32:46 -0700

    Replace WKWebView management with UAMessageCenterMessageViewController

--- src/Airship.Net/Platforms/iOS/Modules/AirshipMessageCenter.cs
+++ src/Airship.Net/Platforms/iOS/Modules/AirshipMessageCenter.cs
@@ -165,7 +165,9 @@ namespace AirshipDotNet.Platforms.iOS.Modules
 
             NSRunLoop.Main.InvokeOnMainThread(() =>
             {
-                AWAirshipWrapper.Shared.MessageCenter.Display();
+                var vc = UAMessageCenterViewControllerFactory.MakeWithTheme(null, null);
+                var top = GetTopViewController();
+                top?.PresentViewController(vc, animated: true, completionHandler: null);
                 tcs.SetResult(true);
             });
 
@@ -187,6 +189,20 @@ namespace AirshipDotNet.Platforms.iOS.Modules
             });
 
             return tcs.Task;
+        }
+
+        private static UIKit.UIViewController? GetTopViewController()
+        {
+            var window = UIKit.UIApplication.SharedApplication.ConnectedScenes
+                .OfType<UIKit.UIWindowScene>()
+                .SelectMany(s => s.Windows)
+                .LastOrDefault(w => w.IsKeyWindow);
+
+            var root = window?.RootViewController;
+            while (root?.PresentedViewController != null)
+                root = root.PresentedViewController;
+
+            return root;
         }
 
         private static DateTime? FromNSDate(NSDate? date)
